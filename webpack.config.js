80d315cfb90292e436c29dca27eea53a732d6a2e
Noam <noamru22@gmail.com>
2021-04-01 16:57:41 +0200

    environment setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+// const webpack = require("webpack");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = () => {
+  return {
+    entry: "./src/index.tsx",
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "build"),
+      compress: true,
+      port: 4000,
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin({
+        async: false,
+        eslint: {
+          files: "./src/**/*",
+        },
+      }),
+    ],
+  };
+};
