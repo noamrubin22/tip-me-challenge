fdd34fd4184541d7e93cbded4df6120c030ccdf9
Noam <noamru22@gmail.com>
2021-04-05 18:58:51 +0200

    [feat]: add oldest established airline

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React, {useEffect, useState} from 'react'
 import enTranslations from '@shopify/polaris/locales/en.json';
 import { Page, Card} from '@shopify/polaris';
 import {fetchAirlinesData,  fetchPassengerData} from "./api/api";
+import PassengerTable from './PassengerTable';
 
 
 interface Airline {
     country: string,
-    established: string,
+    established: string | number,
     head_quarters: string,
     id: number, 
     logo: string,
     name: string,
-    sloagen: string,
+    slogan: string,
     website: string
 }
 
@@ -24,44 +25,119 @@ interface Passenger {
 }
 
 const App = () => {
-    const [airlines, setAirlines] = useState<number>(0);
+    const [airlinesAmount, setAirlinesAmount] = useState<number>(0);
     const [trips, setTrips] = useState<number>(0);
-    const [oldestAirline, setOldestAirline] = useState<Airline>();
+    const [oldestAirline, setOldestAirline] = useState<Airline | undefined>(undefined);
+    const [passengersData, setPassengersData] = useState<Passenger | undefined>(undefined);
 
     useEffect(() => {
         fetchAirlinesData()
             .then(response => {
                 const airlineData = response.data;
-                setAirlines(airlineData.length);
-                
-                const establishedYears:Array<number> = [];
-                const unknownYearList: Array<Airline> = []; 
-                const leftOvers: Array<Airline> = [];
+                setAirlinesAmount(airlineData.length);
+                // const cleanAirlineData = cleanData(airlineData);
+                // setOldestAirline(getSmallestYear(cleanAirlineData);
 
-                setOldestAirline(airlineData.reduce((acc:string, current:Airline, index:number) =>{
-                    if (typeof current.established === 'string') {
-                        const year = Number(current.established);
-                        if (isNaN(year)) {
-                            unknownYearList.push(current)
-                        } else {
-                            establishedYears.push(year)
+                // const cleanData = (airlines:Array<Airline>) : Array<Airline> => {
+                //     const result = airlines.map((airline:Airline, _index:number) => {
+                //         const establishedYear = airline.established;
+    
+                //         if (!establishedYear) {
+                //             return {...airline, year : undefined }
+                //         } else if (typeof establishedYear === "string") {
+                //             if (establishedYear.includes("-")){
+                //                 const year = Number(establishedYear.substring(0,4));
+                //                 return refactorAirlineWithYear(year, airline);
+                //             } else if (establishedYear.includes("/")) {
+                //                 const year = (Number(establishedYear.substring(establishedYear.length - 4)));
+                //                 return refactorAirlineWithYear(year, airline);
+                //             } else if (establishedYear.length < 4) {
+                //                 return {...airline, year : undefined }
+                //             } else if (establishedYear.length === 4) {
+                //                 const year = Number(establishedYear);
+                //                 return refactorAirlineWithYear(year, airline);
+                //             } else if (establishedYear.length > 4) {
+                //                 const date = new Date(establishedYear);
+                //                 const year = date.getFullYear();
+                //                 if (!isNaN(year)) {
+                //                     return refactorAirlineWithYear(year, airline)
+                //                 } else {
+                //                     return {...airline, year : undefined }
+                //                 }
+                //             }
+                //         }
+                //     })
+                //     console.log("claenData", cleanData);
+                //     return []
+                // }
+
+
+                    
+                    // .filter((airline:Airline) => {
+                    //     if (airline.established) {
+                    //         return true
+                    //     } else {
+                    //         return false
+                    //     }
+                    // });
+                // }
+                const validYearsAirlines:Array<Airline> = airlineData.map((airline:Airline, _index:number) => {
+                    const establishedYear = airline.established;
+
+                    if (!establishedYear) {
+                        return false
+                    } else if (typeof establishedYear === "string") {
+                        if (establishedYear.includes("-")){
+                            const year = Number(establishedYear.substring(0,4));
+                            return refactorAirlineWithYear(year, airline);
+                        } else if (establishedYear.includes("/")) {
+                            const year = (Number(establishedYear.substring(establishedYear.length - 4)));
+                            return refactorAirlineWithYear(year, airline);
+                        } else if (establishedYear.length < 4) {
+                            return false
+                        } else if (establishedYear.length === 4) {
+                            const year = Number(establishedYear);
+                            return refactorAirlineWithYear(year, airline);
+                        } else if (establishedYear.length > 4) {
+                            const date = new Date(establishedYear);
+                            const year = date.getFullYear();
+                            if (!isNaN(year)) {
+                                return refactorAirlineWithYear(year, airline)
+                            } else {
+                                return false
+                            }
                         }
-                    } else {
-                        leftOvers.push(current)
                     }
-                    console.log("not a number list", unknownYearList);
-                    console.log("leftovers:", leftOvers);
-                }))
+                }).filter((el:Airline) => {
+                    if (el.established) {
+                        return true
+                    } else {
+                        return false
+                    }
+                });
+                const smallestYear = validYearsAirlines.reduce((acc:Airline|undefined, current:Airline) => {
+                    if (acc === undefined) {
+                        return current;
+                    }
+                    if (Number(acc.established) > Number(current.established)) {
+                        return current;
+                    }
+                    return acc
+                }, undefined)
+                setOldestAirline(smallestYear);
+                
             })  
             .catch((error) => {
                 console.log(error);
             })
-
+            
+            console.log(oldestAirline);
         fetchPassengerData()
             .then(response => {
                 const passengerData = response.data.data;
-
-                setTrips(passengerData.reduce((acc:number, current:Passenger, index:number) => {
+                setPassengersData(passengerData);
+                console.log("passengersdata", passengersData);
+                setTrips(passengerData.reduce((acc:number, current:Passenger, _index:number) => {
                     if((isNaN(current.trips))) {
                         return acc + 0
                     } else {
@@ -74,10 +150,27 @@ const App = () => {
             })
         },[])
 
+        const refactorAirlineWithYear = (year:number, airline:Airline) => {
+            const newAirlineObject = {...airline};
+            newAirlineObject.established = year;
+            return newAirlineObject;
+        }
+
+        const getSmallestYear = (airlines:Array<Airline>) => {
+            return airlines.reduce((acc:Airline|undefined, current:Airline) => {
+                if (acc === undefined) {
+                    return current;
+                }
+                if (Number(acc.established) > Number(current.established)) {
+                    return current;
+                }
+                return acc
+            }, undefined)
+        }
     return (
         <Page title="Airlines data">
             <Card sectioned>
-                <h1>Total amount of airlines: {airlines}</h1>
+                <h1>Total amount of airlines: {airlinesAmount}</h1>
             </Card>
             <Card sectioned>
                 <h1>Total amount of trips: {trips}</h1>
@@ -86,7 +179,10 @@ const App = () => {
                 <h1>Overall sales: €{trips * 199}</h1>
             </Card>
             <Card sectioned>
-                <h1>Establishing year of the oldest airline: </h1>
+                <h1>The oldest airline ({oldestAirline?.name}) is established in {oldestAirline?.established}  </h1>
+            </Card>
+            <Card sectioned>
+                <PassengerTable data={passengersData}/>
             </Card>
         </Page>   
     )
--- src/PassengerTable.tsx
+++ src/PassengerTable.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { Page, Card, DataTable} from '@shopify/polaris';
 
-const PassengerTable = () => {
+const PassengerTable = (props: any) => {
+    const rows = [
+        ['Emerald Silk Gown', '$875.00', 124689, 140, '$122,500.00'],
+        ['Mauve Cashmere Scarf', '$230.00', 124533, 83, '$19,090.00'],
+        [
+          'Navy Merino Wool Blazer with khaki chinos and yellow belt',
+          '$445.00',
+          124518,
+          32,
+          '$14,240.00',
+        ],
+      ];
+
+    
     return (
         <Page title="Passenger data">
             <Card>
-                {/* <DataTable */}
-                {/* /> */}
+                <DataTable 
+                    columnContentTypes={[
+                        "numeric",
+                        "text",
+                        "numeric",
+                        "numeric"
+                    ]}
+                    headings={[
+                        'ID',
+                        "Name",
+                        "Number of trips",
+                        "Total amount paid for flights"
+                    ]}
+                    rows={rows}
+                />
+                
             </Card>
             
         </Page>
